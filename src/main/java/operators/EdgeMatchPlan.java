53f8cb2fd77f0e528a734689edaf38222b7648ce
ravi <ravi.srinivasan99@gmail.com>
2019-12-16 23:57:48 +0100

    Ancora in elaborazione

--- src/main/java/operators/EdgeMatchPlan.java
+++ src/main/java/operators/EdgeMatchPlan.java
@@ -1,10 +1,16 @@
 package operators;
 
+import graph.statistics.Comparators;
 import graph.statistics.Statistics;
 import operators.utils.Constraint;
+import operators.utils.ConstraintType;
 import operators.utils.ConstraintsArrayBuilder;
 import oracle.pgql.lang.ir.QueryEdge;
 import oracle.pgql.lang.ir.QueryExpression;
+import oracle.pgql.lang.ir.QueryVertex;
+import settings.GraphSettings;
+import settings.HardwareCostSettings;
+import settings.Settings;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -16,35 +22,97 @@ import java.util.Set;
 
 public class EdgeMatchPlan extends ConstraintsArrayBuilder implements QueryPlan, Comparable<QueryPlan>{
 
-    private QueryEdge edge;
+    private QueryVertex leftVertex;
+    private QueryVertex rightVertex;
+
+    private QueryEdge edgeToFind;
 
     private QueryPlan child;
-    private QueryPlan parent;
+    private QueryPlan parent1;
+    private QueryPlan parent2;
 
-    private ArrayList<Constraint> constraints;
+    private ArrayList<Constraint> constraintEdgeToFind;
 
     private double operatorCost;
     private int operatorCardinality;
 
+    GraphSettings graphSettings;
+    HardwareCostSettings hardwareCostSettings;
+
     // constructor
-    public EdgeMatchPlan(QueryEdge queryEdge, QueryPlan parentPlan,
-                         Set<QueryExpression> constraintsSet, Statistics statistics) {
+    public EdgeMatchPlan(QueryVertex leftQueryVertex, QueryVertex rightQueryVertex, QueryEdge edgeToFind,
+                                   QueryPlan parentPlan1, Settings settings,
+                                   Set<QueryExpression> constraintsSet, Statistics statistics) {
 
-        this.parent = parentPlan;
-        this.edge = queryEdge;
-        this.constraints = constraintsEdgeArrayBuilder(queryEdge, constraintsSet);
+        this.parent1 = parentPlan1;
+        if (parent1 instanceof CartesianProductPlan) {
+            this.parent2 = null;
+        } else {
+            this.parent2 = this.parent1.getParentPlan();
+        }
 
-        // aggiungere costo
+        this.leftVertex = leftQueryVertex;
+        this.rightVertex = rightQueryVertex;
+
+        this.edgeToFind = edgeToFind;
+
+        this.graphSettings = settings.getGraphSettings();
+        this.hardwareCostSettings = settings.getHardwareCostSettings();
+
+        this.constraintEdgeToFind = constraintsEdgeArrayBuilder(edgeToFind, constraintsSet);
+
+        this.operatorCost = computeCost(statistics);
 
     }
 
+    // computes cost of the operator
     @Override
-    public void computeCardinality(Statistics statistics) {
+    public double computeCost(Statistics statistics) { // da rivedere
+
+        double indexEdgeCost = this.hardwareCostSettings.getIndexEdgeCost();
+        double cpuOperationCost = this.hardwareCostSettings.getCpuOperationCost();
+        double vertexPropertyCost = this.hardwareCostSettings.getVertexPropertyCost();
+
+        int leftVertexCardinality = parent1.getCardinality();
+        int rightVertexCardinality;
+        if (parent2 == null) {
+            rightVertexCardinality = parent1.getCardinality();
+        } else {
+            rightVertexCardinality = parent2.getCardinality();
+        }
+
+        // aggiungere cost binary search
+        return 1;
 
     }
 
-    @Override
-    public void computeCost(Statistics statistics) {
+    // computes total cardinality of given vertex constraints
+    private int computeTotalVertexCardinality(ArrayList<Constraint> constraints, Statistics statistics, int totalCardinality){
+
+        double selectivity = 1;
+
+        for (Constraint constraint: constraints) {
+            selectivity *= computeConstraintSelectivity(constraint, statistics);
+        }
+
+        return (int) (totalCardinality * selectivity);
+
+    }
+
+    // computes selectivity of one vertex constraint
+    private double computeConstraintSelectivity(Constraint constraint, Statistics statistics) {
+
+        if (constraint.getType() == ConstraintType.HAS_LABEL) {
+            String vertexLabel = graphSettings.getVertexTypeCsvHeader();
+            String label = constraint.getAttribute();
+            return statistics.getVertexFilterSelectivity(vertexLabel, label, Comparators.EQUAL);
+        } else if (constraint.getType() == ConstraintType.WHERE) {
+            String attribute = constraint.getAttribute();
+            String value = constraint.getValue();
+            return statistics.getVertexFilterSelectivity(attribute, value, constraint.getConstraintComparator());
+        } else {
+            return 1; // shouldn't enter else statement
+        }
 
     }
 
@@ -56,7 +124,11 @@ public class EdgeMatchPlan extends ConstraintsArrayBuilder implements QueryPlan,
 
     @Override
     public void setParentPlan(QueryPlan parentPlan) {
-        this.parent = parentPlan;
+        this.parent1 = parentPlan;
+    }
+
+    public void setParent2(QueryPlan parent2) {
+        this.parent2 = parent2;
     }
 
     @Override
@@ -66,13 +138,27 @@ public class EdgeMatchPlan extends ConstraintsArrayBuilder implements QueryPlan,
 
     // getters
     @Override
-    public double getCardinality() {
+    public int getCardinality() {
         return operatorCardinality;
     }
 
     @Override
     public double getOperatorCost() {
         return operatorCost;
+    }
+
+    @Override
+    public QueryPlan getParentPlan() {
+        return null;
+    }
+
+    public QueryPlan getParent2() {
+        return parent2;
+    }
+
+    @Override
+    public QueryPlan getChildPlan() {
+        return null;
     }
 
     // compare operators on costs
